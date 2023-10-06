6906cbfa68f442adfe0effc95f39613fc76af5a7
Anderson-Andre-P <eleu.ander@gmail.com>
2023-10-05 21:47:26 -0300

    feat: upload project

---
+++ README.md
@@ -0,0 +1,3 @@
+# onboarding_and_signin
+
+This app was created just to have fun when I have time off from work.
---
+++ android/app/src/main/kotlin/app/anderson/andre/onboarding_and_signin/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.anderson.andre.onboarding_and_signin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
